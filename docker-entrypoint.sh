111fffcaa9142629cd65ca6f6c847470357bd7ab
Alfie-mx <dev@alfredordz.com>
2017-08-07 17:47:55 -0700

    Debugging options

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,6 +29,28 @@ $WORDPRESS_CONFIG_EXTRA
 PHP"
 fi
 
+# Make sure uploads directory exists and is writeable.
+chown www-data:www-data /var/www/html/wp-content
+runuser www-data -s /bin/sh -c "mkdir -p /var/www/html/wp-content/uploads"
+
+# MySQL may not be ready when container starts.
+set +ex
+while true; do
+  if curl --fail --show-error --silent "${WORDPRESS_DB_HOST:-mysql}:3306" > /dev/null 2>&1; then break; fi
+  echo "Waiting for MySQL to be ready...."
+  sleep 3
+done
+set -ex
+
+# Install WordPress.
+runuser www-data -s /bin/sh -c "\
+wp core $([ "$WORDPRESS_INSTALL_TYPE" == "multisite" ] && echo "multisite-install" || echo "install") \
+  --title=\"${WORDPRESS_SITE_TITLE:-Project}\" \
+  --admin_user=\"${WORDPRESS_SITE_USER:-wordpress}\" \
+  --admin_password=\"${WORDPRESS_SITE_PASSWORD:-wordpress}\" \
+  --admin_email=\"${WORDPRESS_SITE_EMAIL:-admin@example.com}\" \
+  --url=\"${WORDPRESS_SITE_URL:-http://project.local}\" \
+  --skip-email"
 
 if ! [ -f /tmp/db-dump/*.sql ]; then
    runuser www-data -s /bin/sh -c "\
