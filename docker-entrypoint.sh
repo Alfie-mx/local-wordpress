1aeacf38ce942e8e27de80bae19286f6576d849d
Alfie-mx <dev@alfredordz.com>
2017-11-16 15:06:32 -0800

    Testing search and replace

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,11 +20,12 @@ $WORDPRESS_CONFIG_EXTRA
 PHP"
 fi
 
-#if ! [ -f /tmp/db-dump/*.sql ]; then
-#   runuser www-data -s /bin/sh -c "\
-#   wp search-replace \"${DB_SEARCH}\" \"${DB_REPLACE}\" \
-#   "
-#fi
+# Search and Replace in DB to replace domain name.
+if ! [ -f /tmp/db-dump/*.sql ]; then
+   runuser www-data -s /bin/sh -c "\
+   wp search-replace \"${DB_SEARCH}\" \"${DB_REPLACE}\" \
+   "
+fi
 
 
 exec "$@"
