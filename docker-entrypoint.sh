7b12cf3f70fe81e5dfe349afb701775d0a648e1e
Alfie-mx <dev@alfredordz.com>
2017-11-21 19:49:32 -0800

    wp info

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,35 +8,41 @@ WEB_USER=www-data
 echo "==========================================================================="
 echo ${ROOT_DIR}
 echo ${WEB_USER}
+printf "\n"
 
 
-# Create WordPress config.
-if ! [ -f ${ROOT_DIR}/wp-config.php ]; then
-  runuser ${WEB_USER} -s /bin/sh -c "\
-  wp config create \
-    --dbhost=\"${WORDPRESS_DB_HOST:-mysql}\" \
-    --dbname=\"${WORDPRESS_DB_NAME:-wordpress}\" \
-    --dbuser=\"${WORDPRESS_DB_USER:-root}\" \
-    --dbpass=\"${WORDPRESS_DB_PASSWORD}\" \
-    --skip-check \
-    --extra-php <<PHP
-${WORDPRESS_CONFIG_EXTRA}
-PHP"
-fi
+wp --info
 
-# MySQL may not be ready when container starts.
-set +ex
-while true; do
-  if curl --fail --show-error --silent "db:3306" > /dev/null 2>&1; then break; fi
-  echo "Waiting for MySQL to be ready...."
-  sleep 3
-done
-set -ex
+## Create WordPress config.
+#if ! [ -f ${ROOT_DIR}/wp-config.php ]; then
+#  runuser ${WEB_USER} -s /bin/sh -c "\
+#  wp config create \
+#    --dbhost=\"${WORDPRESS_DB_HOST:-mysql}\" \
+#    --dbname=\"${WORDPRESS_DB_NAME:-wordpress}\" \
+#    --dbuser=\"${WORDPRESS_DB_USER:-root}\" \
+#    --dbpass=\"${WORDPRESS_DB_PASSWORD}\" \
+#    --skip-check \
+#    --extra-php <<PHP
+#${WORDPRESS_CONFIG_EXTRA}
+#PHP"
+#fi
 
-# Install WordPress.
-runuser ${WEB_USER} -s /bin/sh -c "\
-wp core download
-"
+
+## MySQL may not be ready when container starts.
+#set +ex
+#while true; do
+#  if curl --fail --show-error --silent "db:3306" > /dev/null 2>&1; then break; fi
+#  echo "Waiting for MySQL to be ready...."
+#  sleep 3
+#done
+#set -ex
+
+
+## Install WordPress.
+#runuser ${WEB_USER} -s /bin/sh -c "\
+#wp core download
+#"
+
 
 #runuser $WEB_USER -s /bin/sh -c "\
 #wp core $([ "${WORDPRESS_INSTALL_TYPE}" == "multisite" ] && echo "multisite-install" || echo "install") \
@@ -46,6 +52,7 @@ wp core download
 #  --admin_email=\"${WORDPRESS_SITE_EMAIL:-admin@example.com}\" \
 #  --url=\"${WORDPRESS_SITE_URL:-http://project.dev}\" \
 #  --skip-email"
+
 
 if [ -n "${DB_SEARCH}" ]; then
 runuser ${WEB_USER} -s /bin/sh -c "\
