7cd11da5423e9f5d3ec33c08c08e38256137066f
Alfie-mx <dev@alfredordz.com>
2017-11-21 20:22:07 -0800

    starting little by little

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,19 +13,20 @@ printf "\n"
 
 wp --info
 
-## Create WordPress config.
-#if ! [ -f ${ROOT_DIR}/wp-config.php ]; then
-#  runuser ${WEB_USER} -s /bin/sh -c "\
-#  wp config create \
-#    --dbhost=\"${WORDPRESS_DB_HOST:-mysql}\" \
-#    --dbname=\"${WORDPRESS_DB_NAME:-wordpress}\" \
-#    --dbuser=\"${WORDPRESS_DB_USER:-root}\" \
-#    --dbpass=\"${WORDPRESS_DB_PASSWORD}\" \
-#    --skip-check \
-#    --extra-php <<PHP
-#${WORDPRESS_CONFIG_EXTRA}
-#PHP"
-#fi
+
+# Create WordPress config.
+if ! [ -f ${ROOT_DIR}/wp-config.php ]; then
+  runuser ${WEB_USER} -s /bin/sh -c "\
+  wp config create \
+    --dbhost=\"${WORDPRESS_DB_HOST:-mysql}\" \
+    --dbname=\"${WORDPRESS_DB_NAME:-wordpress}\" \
+    --dbuser=\"${WORDPRESS_DB_USER:-root}\" \
+    --dbpass=\"${WORDPRESS_DB_PASSWORD}\" \
+    --skip-check \
+    --extra-php <<PHP
+${WORDPRESS_CONFIG_EXTRA}
+PHP"
+fi
 
 
 ## MySQL may not be ready when container starts.
@@ -54,12 +55,12 @@ wp --info
 #  --skip-email"
 
 
-if [ -n "${DB_SEARCH}" ]; then
-runuser ${WEB_USER} -s /bin/sh -c "\
-wp search-replace \"${DB_SEARCH}\" \"${DB_REPLACE}\" \
- --allow-root \
-"
-fi
+#if [ -n "${DB_SEARCH}" ]; then
+#runuser ${WEB_USER} -s /bin/sh -c "\
+#wp search-replace \"${DB_SEARCH}\" \"${DB_REPLACE}\" \
+# --allow-root \
+#"
+#fi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
